15e68268c155425ece74ca60d5524f9bd260fd48
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-10-02 04:58:39 +0900

    Update plot_csv_live_20160927_eval

--- scripts/plot_csv_live_20160927_eval.py
+++ scripts/plot_csv_live_20160927_eval.py
@@ -7,40 +7,45 @@ import pandas as pd
 import seaborn as sns
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('csv_file')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv_file')
+    args = parser.parse_args()
 
-csv_file = args.csv_file
+    csv_file = args.csv_file
 
-df = pd.DataFrame.from_csv(csv_file, index_col=None)
-df = df.sort(columns='stamp')
-print(df.columns)
+    df = pd.DataFrame.from_csv(csv_file, index_col=None)
+    df = df[:-70]
+    print(df.columns)
 
-colors = sns.husl_palette(4, l=.5, s=.5)
+    colors = sns.husl_palette(4, l=.5, s=.5)
 
-t_start = df.iloc[0].stamp
+    t_start = df.iloc[0].stamp
+    elapsed_time = (df.stamp - t_start) * 1e-9
 
-ax1 = plt.subplot(211)
-p1 = plt.plot(df.stamp - t_start, df.iu, color=colors[0], label='IU')
-plt.legend(loc='center left')
-plt.ylabel('IU')
+    ax1 = plt.subplot(211)
+    p1 = plt.plot(elapsed_time, df.iu, color=colors[0], label='IU')
+    plt.legend(loc='center left')
+    plt.ylabel('IU')
 
-ax2 = ax1.twinx()
-ax2.set_ylabel('Volume [$m^2$]')
-ax2.plot(df.stamp - t_start, df.volume_tp,
-         color=colors[1], label='$V_{tp}$')
-ax2.plot(df.stamp - t_start, df.volume_fn,
-         color=colors[2], label='$V_{fn}$')
-ax2.plot(df.stamp - t_start, df.volume_fp,
-         color=colors[3], label='$V_{fp}$')
-plt.legend(loc='center right')
+    ax2 = ax1.twinx()
+    ax2.plot(elapsed_time, df.volume_tp * 1e3,
+             color=colors[1], label='$V_{tp}$')
+    ax2.plot(elapsed_time, df.volume_fn * 1e3,
+             color=colors[2], label='$V_{fn}$')
+    ax2.plot(elapsed_time, df.volume_fp * 1e3,
+             color=colors[3], label='$V_{fp}$')
+    ax2.set_ylabel('Volume [$10^-3 m^2$]')
+    plt.legend(loc='center right')
 
-plt.subplot(212)
-plt.plot(df.stamp - t_start, df.camera_velocity, marker='o',
-         color=colors[0], label='camera velocity')
-plt.legend(loc='upper right')
-plt.xlabel('Time [ns]')
-plt.ylabel('Camera Velocity')
+    plt.subplot(212)
+    plt.plot(elapsed_time, df.camera_velocity * 1e3,
+             marker='o', color=colors[0], label='camera velocity')
+    plt.xlabel('Time [sec]')
+    plt.ylabel('Camera Velocity [$10^-3$ m/s]')
 
-plt.show()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
